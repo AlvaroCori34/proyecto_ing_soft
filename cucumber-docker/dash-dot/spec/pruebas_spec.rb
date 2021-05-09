30152040f96a82ed07da48a465d4d38f15d2e761
AlvaroCori <bacs200018@gmail.com>
2021-05-09 19:03:24 -0400

    prueba de cantidad de casillas

--- cucumber-docker/dash-dot/spec/pruebas_spec.rb
+++ cucumber-docker/dash-dot/spec/pruebas_spec.rb
@@ -1,20 +1,24 @@
 require "./lib/MapOfRobot.rb"
 require "./lib/Square.rb"
 RSpec.describe "Pruebas del robot y su mapa" do
-    it "deberia devolver nil" do
+    it "deberia devolver para una casilla recien creada nil" do
         #ebs= MapOfRobot.new(2,2)
         ebs = Square.new()
         expect(ebs.getObject()).to eq nil
     end
-    it "deberia devolver cantidad de las filas" do
+    it "deberia devolver cantidad de las filas del mapa" do
         map= MapOfRobot.new(3,4)
         row, col = map.shape()
         expect(row).to eq 3
     end
-    it "deberia devolver cantidad de las columnas" do
+    it "deberia devolver cantidad de las columnas del mapa" do
         map= MapOfRobot.new(2,5)
         row, col = map.shape()
         expect(col).to eq 5
+    end
+    it "deberia devolver cantidad casillas" do
+        map= MapOfRobot.new(2,5)
+        expect(map.getSizeMatrix()).to eq 10
     end
     #column_size
     #column_count
